a6a73c2da39914dbd2a2f788fd8e57d3597ede38
Mikita.Sharshun <Mikita.Sharshun@WSD-034-71>
2020-11-10 19:26:33 +0300

    add auction repository

---
+++ OnlineAuction/Engine/IAuctionRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OnlineAuction.Models;
+
+namespace OnlineAuction.Engine
+{
+    public interface IAuctionRepository
+    {
+        IQueryable<Lot> Lots { get; }
+        Task<Lot> AddNewLotAsync(Lot lot);
+        Task<bool> TryDeleteLotAsync(int id);
+        Task<Lot> UpdateLotAsync(Lot lot);
+        Task<Lot> GetLotAsync(int id);
+    }
+}
