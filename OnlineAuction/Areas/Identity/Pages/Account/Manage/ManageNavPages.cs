f096e674aa77f5f32f12f7fc29238325453c56d0
Mikita.Sharshun <Mikita.Sharshun@WSD-034-71>
2020-12-01 19:32:56 +0300

    add winning lot message to profile

--- OnlineAuction/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ OnlineAuction/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -14,6 +14,8 @@ namespace OnlineAuction.Areas.Identity.Pages.Account.Manage
 
         public static string ChangePassword => "ChangePassword";
 
+        public static string WinningLots => "WinningLots";
+
         public static string DownloadPersonalData => "DownloadPersonalData";
 
         public static string DeletePersonalData => "DeletePersonalData";
@@ -29,6 +31,7 @@ namespace OnlineAuction.Areas.Identity.Pages.Account.Manage
         public static string EmailNavClass(ViewContext viewContext) => PageNavClass(viewContext, Email);
 
         public static string ChangePasswordNavClass(ViewContext viewContext) => PageNavClass(viewContext, ChangePassword);
+        public static string WinningLotsNavClass(ViewContext viewContext) => PageNavClass(viewContext, WinningLots);
 
         public static string DownloadPersonalDataNavClass(ViewContext viewContext) => PageNavClass(viewContext, DownloadPersonalData);
 
--- OnlineAuction/Engine/EmailService.cs
+++ OnlineAuction/Engine/EmailService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MailKit.Net.Smtp;
+using Microsoft.Extensions.Configuration;
 using MimeKit;
 using OnlineAuction.Models;
 
@@ -10,17 +11,27 @@ namespace OnlineAuction.Engine
 {
     public class EmailService : IEmailService
     {
+        private readonly string _emailSender;
+        private readonly string _emailSenderPassword;
+        public EmailService(IConfiguration configuration)
+        {
+            _emailSender = configuration.GetConnectionString("EmailSender");
+            _emailSenderPassword = configuration.GetConnectionString("EmailSenderPassword");
+        }
+
+        private const string TitleClasses = "\"font-size: 1.4rem;\"";
+
         public async Task SendEmailToWinnerAsync(string email,string name, Lot lot, int price)
         {
             var emailMessage = new MimeMessage();
             
-            emailMessage.From.Add(new MailboxAddress("Online Auction", "online.auction.sharshun@gmail.com"));
+            emailMessage.From.Add(new MailboxAddress("Online Auction", _emailSender));
             emailMessage.To.Add(new MailboxAddress("", "sharshoon321@gmail.com"));
             emailMessage.Subject = "Winner instructions";
             emailMessage.Body = new BodyBuilder
             {
-                HtmlBody = $"<h1>Hello {name}</h1>" +
-                           $"<p>We are pleased to inform you that you have become the winner of the {lot.Name } lot</p>" +
+                HtmlBody = $"<h1 style={TitleClasses}>Hello {name}</h1>" +
+                           $"<p>We are pleased to inform you that you have become the winner of the {lot.Name} lot</p>" +
                            "<p>Lot characteristics:</p>" +
                            "<ul>" +
                            $"   <li>Name: {lot.Name}</li>" +
@@ -33,7 +44,7 @@ namespace OnlineAuction.Engine
             using var client = new SmtpClient();
             await client.ConnectAsync("smtp.gmail.com", 587);
             client.AuthenticationMechanisms.Remove("XOAUTH2");
-            await client.AuthenticateAsync("online.auction.sharshun@gmail.com", "onlineauction");
+            await client.AuthenticateAsync(_emailSender, _emailSenderPassword);
             await client.SendAsync(emailMessage);
 
             await client.DisconnectAsync(true);
